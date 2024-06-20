147eb967c6a1eade90423f7334a4427dabaeb71e
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2024-06-20 16:30:12 +0200

    Add files via upload

---
+++ GTA San Andreas Cheat Menu Using Game Guardians.lua
@@ -0,0 +1,185 @@
+-- FOG HORIZON LIGHT
+fogHorizonLight = 1
+
+-- TEXTURE BIT HACK/FPS INCREASE - EDIT TO 40
+textureBitHack = 40
+
+-- INCREASE AND DECREASE GAME SPEED
+gameSpeed = 1.5
+
+-- HUB OPACITY
+hubOpacity = 0.5
+
+-- HEALTH BAR INCREASE
+healthBarIncrease = 200
+
+-- HEALTH BAR INCREASE 2.0
+healthBarIncrease2 = 300
+
+-- CJ CONTROL SIZE
+cjControlSize = 1.2
+
+-- CJ CONTROL SENSITIVITY
+cjControlSensitivity = 2.0
+
+-- MAP SHADOW
+mapShadow = true
+
+-- TRAFFIC STOP DISTANCE DETECTION
+trafficStopDistanceDetection = 50
+
+-- ANTI TELEPORT FOR PED IN CAR
+antiTeleportForPedInCar = true
+
+-- TRAFFIC FRAMES
+trafficFrames = 30
+
+-- PEDESTRIAN DOESN'T BRAKE THE CAR 2
+pedestrianDoesntBrakeTheCar2 = true
+
+-- PEDESTRIAN DOES NOT BRAKE VEHICLE
+pedestrianDoesNotBrakeVehicle = true
+
+-- CRAZY PED ON CAR
+crazyPedOnCar = true
+
+-- DUMB DRIVER
+dumbDriver = true
+
+-- TRAFFIC CAR SPEED BOOST
+trafficCarSpeedBoost = 2.0
+
+-- TRAFFIC VEHICLE SPEED MULT
+trafficVehicleSpeedMult = 3.0
+
+-- PARTICLES COLOR CHANGER
+particlesColorChanger = "red"
+
+-- WATER PARTICLE
+waterParticle = true
+
+-- PARTÍCULAS (4)
+particulas4 = true
+
+-- CJ IMORTAL
+cjImmortal = true
+
+-- SPEEDHACK 1
+speedHack1 = 2.0
+
+-- DISTANCED SOUNDS VOLUME (0 - High)
+distancedSoundsVolume = 50
+
+-- CRASHES SOUNDS VOLUME
+crashesSoundsVolume = 100
+
+-- BRAKES VOLUME
+brakesVolume = 50
+
+-- VEHICLE ENGINE VOLUME
+vehicleEngineVolume = 100
+
+-- CAMERA HEIGHT (VEHICLE)
+cameraHeightVehicle = 2.0
+
+-- VEHICLE FOV
+vehicleFov = 90
+
+-- MOTORCYCLE FOV
+motorcycleFov = 80
+
+-- VEHICLE CAMERA SENSITIVITY
+vehicleCameraSensitivity = 2.0
+
+-- VEHICLE CAMERA NEW STYLE
+vehicleCameraNewStyle = true
+
+-- FOV
+fov = 90
+
+-- SENSITIVITY
+sensitivity = 2.0
+
+-- NEW FOV STYLE
+newFovStyle = true
+
+-- CAMERA HACK
+cameraHack = true
+
+-- CHARACTER ANIMATION SPEED
+characterAnimationSpeed = 2.0
+
+-- PARTICLES ANIMATION SPEED
+particlesAnimationSpeed = 3.0
+
+-- SHADOW INTANSE ADJUST 1
+shadowIntanseAdjust1 = 2.0
+
+-- SHADOW INTANSE ADJUST 2
+shadowIntanseAdjust2 = 3.0
+
+-- VEHICLE SPEEDHACK
+vehicleSpeedHack = 2.0
+
+-- GHOST PLAYER
+ghostPlayer = true
+
+-- INVISIBLE CAR
+invisibleCar = true
+
+-- SLIDE
+slide = true
+
+-- VEHICLE GRAVITY 2
+vehicleGravity2 = 2.0
+
+-- VEHICLE COLLISION MULTIPLIER 2.0
+vehicleCollisionMultiplier2 = 3.0
+
+-- VEHICLE COLLISION MULTIPLIER
+vehicleCollisionMultiplier = 2.0
+
+-- CAR GRAVITY
+carGravity = 2.0
+
+-- VIEW DISTANCE/FPS BOOST (2)
+viewDistanceFpsBoost = 2.0
+
+-- CHARACTER SPECULAR REFLECTION 1
+characterSpecularReflection1 = true
+
+-- CHARACTER SPECULAR REFLECTION 2
+characterSpecularReflection2 = true
+
+-- CAR SPECULAR REFLECTION 1
+carSpecularReflection1 = true
+
+-- CAR SPECULAR REFLECTION 2
+carSpecularReflection2 = true
+
+-- ITEM VIEW DISTANCE
+itemViewDistance = 500
+
+-- ATTACK SPEED
+attackSpeed = 2.0
+
+-- FOG REMOVE/ADD
+fogRemoveAdd = true
+
+-- HOT/COLD COLORIZATION CHANGE
+hotColdColorizationChange = true
+
+-- DISTANCE VIEW FOG CHANGE (1 to H.devices) (-9 to L.devices)
+distanceViewFogChange = 2.0
+
+-- HOT AND ICE COLOR ADJUSTER
+hotAndIceColorAdjuster = true
+
+-- HOT&ICE SHINE ADJUST
+hotAndIceShineAdjust = true
+
+-- SHINE OF ALL OBJECTS
+shineOfAllObjects = true
+
+-- INCREASE AND DECREASE GAME SPEED 2
+increaseAnd
